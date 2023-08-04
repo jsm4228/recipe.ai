d9ce4ce97d5b0024c67cd1666aadd28488a4dbee
Josh Mathew <jsmathew41@gmail.com>
2023-08-04 10:12:54 -0500

    updated config

--- recipe.ai/vite.config.ts
+++ recipe.ai/vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: "0.0.0.0",
+    port: parseInt(process.env.PORT, 10) || 5173, // Parse the port value to a number
+  },
+});
